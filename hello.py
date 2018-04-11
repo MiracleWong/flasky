5ec5cdb0163e70a8211544d93213d050acff425d
MiracleWong <cfwr1991@126.com>
2018-04-11 08:48:37 +0800

    Add the templates

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from flask import request
 from flask import make_response
+from flask import render_template
 from flask.ext.script import Manager
 
 app = Flask(__name__)
@@ -12,15 +13,16 @@ manager = Manager(app)
 @app.route('/')
 def index():
     # return '<h1>Hello World</h1>'
-    #user_agent = request.headers.get('User-Agent')
-    #return '<p>Your browser is %s</p>' % user_agent
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
+    # user_agent = request.headers.get('User-Agent')
+    # return '<p>Your browser is %s</p>' % user_agent
+    # response = make_response('<h1>This document carries a cookie!</h1>')
+    # response.set_cookie('answer', '42')
+    # return response
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     # app.run(debug=True)
