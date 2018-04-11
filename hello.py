de0601bcc59fdb9d2d8eb02e7d2c944c7c72b4db
MiracleWong <cfwr1991@126.com>
2018-04-11 09:57:44 +0800

    add the flask-bootstrap to beautify pages

--- hello.py
+++ hello.py
@@ -6,9 +6,11 @@ from flask import request
 from flask import make_response
 from flask import render_template
 from flask.ext.script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -23,6 +25,14 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_find(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     # app.run(debug=True)
