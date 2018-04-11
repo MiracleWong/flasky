d8dd03ebfcc68df9f9a28050d92d1f8a521e8bf1
MiracleWong <cfwr1991@126.com>
2018-04-11 16:28:39 +0800

    加入Bootstrap的Flash信息

--- hello.py
+++ hello.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env/python
 # -*- encoding: utf-8 -*-
 
-from flask import Flask
-from flask import request
-from flask import make_response
-from flask import render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 # from flask.ext.script import Manager, Server
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required, DataRequired
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -28,18 +24,14 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # return '<h1>Hello M iracle</h1>'
-    # user_agent = request.headers.get('User-Agent')
-    # return '<p>Your browser is %s</p>' % user_agent
-    # response = make_response('<h1>This document carries a cookie!</h1>')
-    # response.set_cookie('answer', '42')
-    # return response
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        old_name = session.get('name')
+        if old_name != form.name.data:
+            flash('Looks like your have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
