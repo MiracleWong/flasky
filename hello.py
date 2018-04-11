b9b2fbbbeb3bd857f9bd6f469cb54759e0a5eafc
MiracleWong <cfwr1991@126.com>
2018-04-11 15:32:33 +0800

    加入FlaskForm，实现表单NameForm

--- hello.py
+++ hello.py
@@ -5,18 +5,28 @@ from flask import Flask
 from flask import request
 from flask import make_response
 from flask import render_template
-from flask.ext.script import Manager, Server
+# from flask.ext.script import Manager, Server
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, DataRequired
 from datetime import datetime
 
 app = Flask(__name__)
-manager = Manager(app)
-manager.add_command("runserver", Server(use_debugger=True))
+app.config['SECRET_KEY'] = 'hard to guess string'
+# manager = Manager(app)
+# manager.add_command("runserver", Server(use_debugger=True))
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # return '<h1>Hello M iracle</h1>'
     # user_agent = request.headers.get('User-Agent')
@@ -24,21 +34,28 @@ def index():
     # response = make_response('<h1>This document carries a cookie!</h1>')
     # response.set_cookie('answer', '42')
     # return response
-    print(datetime.utcnow())
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+
 @app.errorhandler(404)
 def page_not_find(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    manager.run()
+    app.run(debug=True)
+    # manager.run()
