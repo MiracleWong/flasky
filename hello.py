78ca439f1a1f9a0f37916c864cc82e7d39cd199d
MiracleWong <cfwr1991@126.com>
2018-04-11 11:02:05 +0800

    add the Flask-Moment to show datetime

--- hello.py
+++ hello.py
@@ -5,22 +5,27 @@ from flask import Flask
 from flask import request
 from flask import make_response
 from flask import render_template
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Server
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 manager = Manager(app)
+manager.add_command("runserver", Server(use_debugger=True))
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    # return '<h1>Hello World</h1>'
+    # return '<h1>Hello M iracle</h1>'
     # user_agent = request.headers.get('User-Agent')
     # return '<p>Your browser is %s</p>' % user_agent
     # response = make_response('<h1>This document carries a cookie!</h1>')
     # response.set_cookie('answer', '42')
     # return response
-    return render_template('index.html')
+    print(datetime.utcnow())
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
