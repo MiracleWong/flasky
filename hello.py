414a94892a774bbd464ebbdcbfb3a2138ab6ca5f
MiracleWong <cfwr1991@126.com>
2018-04-14 10:10:20 +0800

    Database models with Flask-SQLAlchemy

--- hello.py
+++ hello.py
@@ -15,8 +15,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 print("basedir：", basedir)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # manager = Manager(app)
 # manager.add_command("runserver", Server(use_debugger=True))
@@ -29,6 +31,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    user = db.relationship('User', backref='role')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -38,6 +41,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     
     def __repr__(self):
         return '<User %r>' % self.username
