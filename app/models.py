d7f6bcf596481451dbd5b59c43eed50e4d7a38a4
MiracleWong <cfwr1991@126.com>
2018-04-30 12:12:48 +0800

    第8章：通过Flask-Login进行登录和登出

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@
 
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import login_manager
+from flask_login import UserMixin
 
 
 class Role(db.Model):
@@ -14,9 +16,10 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
@@ -34,3 +37,10 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
